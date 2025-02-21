5732ed719612ebaf528e6c65a1412ee115db338a
PBinush <pasidbinush@gmail.com>
2025-02-22 04:33:28 +0530

    first commit

---
+++ src/main/java/edu/example/Main.java
@@ -0,0 +1,11 @@
+package edu.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class);
+    }
+}
